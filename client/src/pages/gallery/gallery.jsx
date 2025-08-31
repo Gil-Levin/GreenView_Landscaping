49c3dbb1278e594f10d88e4073a8d83aecf0f1a1
Gil-Levin <gil.levin05@gmail.com>
2025-08-31 13:33:18 -0600

    More small fixes

--- client/src/pages/gallery/gallery.jsx
+++ client/src/pages/gallery/gallery.jsx
@@ -89,18 +89,16 @@ export default function Gallery() {
               alt={'image'}
               className="lightbox-image"
             />
-            <div className="lightbox-caption">
-              <h3></h3>
-            </div>
+            <button 
+              className="lightbox-close"
+              onClick={closeLightbox}
+              aria-label="Close image"
+            >
+              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M18 6L6 18M6 6L18 18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+            </button>
           </div>
-          <button 
-            className="lightbox-close"
-            onClick={closeLightbox}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" className="close-icon" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
         </div>
       )}
     </div>
